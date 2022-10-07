c4f9b1d1a5f61cdfb89b208e439e21de940bcca3
Stefan Petrov <stefspetrov@gmail.com>
2022-10-07 12:35:15 +0300

    add modal overlay

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
-      <Header/>
+      <Cart />
+      <Header />
       <main>
         <Meals />
       </main>
